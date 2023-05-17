4a3e0f7baceda787177f6f4c35fe35d99c18602f
FrensiM <frensimeta@gmail.com>
2023-05-17 02:20:18 -0700

    is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - fun that check is a mode is a leaf
+ * @node: node that will check
+ * Return: 1 or 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
